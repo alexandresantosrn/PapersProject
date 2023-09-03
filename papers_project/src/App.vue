6d0779c85323a29559a42e3b763ee2660768eae6
Alexandre Dantas dos Santos <allexkid@info.ufrn.br>
2023-09-03 11:17:10 -0300

    Adicionando footer.

--- papers_project/src/App.vue
+++ papers_project/src/App.vue
@@ -1,8 +1,39 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div id="app">
+    <Footer />
+  </div>
+  
 </template>
 
+<script>
+import Footer from './components/Footer.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Footer
+  }
+  
+}
+</script>
+
+<style>
+  * {
+    font-family: HELVETICA;
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+
+  .main-container{
+     margin: 50px;
+     min-height: 250px;
+  }
+
+  h1{
+    text-align: center;
+    font-size: 42px;
+    margin-bottom: 30px;
+    color: #222
+  }
+</style>
