19822e21e0d378cba202293d66b815f3d7e603da
Alexandre Dantas dos Santos <allexkid@info.ufrn.br>
2023-09-03 11:40:24 -0300

    Adicionando Navbar.

--- papers_project/src/App.vue
+++ papers_project/src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
+    <Navbar :logo="logo_src" :alt="app_name"/>
     <Footer />
   </div>
   
 </template>
 
 <script>
+import Navbar from './components/Navbar.vue';
 import Footer from './components/Footer.vue';
 
 export default {
   name: 'App',
   components: {
+    Navbar,
     Footer
   },
   data() {
