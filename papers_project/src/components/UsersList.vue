d94af2265302d7c742eef91f085f9445f3d7f5cf
Alexandre Dantas dos Santos <allexkid@info.ufrn.br>
2023-12-04 21:42:43 -0300

    Corrigindo bug.

--- papers_project/src/components/UsersList.vue
+++ papers_project/src/components/UsersList.vue
@@ -28,15 +28,15 @@ export default {
         }
     },
     methods: {
-        async getUsuarios() {
-           //const req = await fetch('http://192.168.2.115:8080/usuarios');    
-           const req = await fetch('http://localhost:8080/usuarios');              
-           const data = await req.json();
-           this.users = data;           
-        }
+        // async getUsuarios() {
+        //    //const req = await fetch('http://192.168.2.115:8080/usuarios');    
+        //    const req = await fetch('http://localhost:8080/usuarios');              
+        //    const data = await req.json();
+        //    this.users = data;           
+        // }
     },
     mounted() {
-        this.getUsuarios()
+        //this.getUsuarios()
     },
     // updated() {
     //     this.getUsuarios();
